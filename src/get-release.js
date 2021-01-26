c7df65f16de2a45ed9e51bc5e7bfb629ca1061c8
Harel Ben-Attia <harel@coralogix.com>
2021-01-26 10:14:15 +0200

    choose tag as input

--- src/get-release.js
+++ src/get-release.js
@@ -10,7 +10,7 @@ async function run() {
     const { owner, repo } = context.repo;
 
     // Get the tag name from the triggered action
-    const tagName = context.ref;
+    const tagName = core.getInput('tag');
 
     // This removes the 'refs/tags' portion of the string, i.e. from 'refs/tags/v1.10.15' to 'v1.10.15'
     const tag = tagName.replace("refs/tags/", "");
